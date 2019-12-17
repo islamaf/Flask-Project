605901c9ef3b4f9d7acef2c8d54ce7f8c86c6d6f
islamaf <44944648+islamaf@users.noreply.github.com>
2019-12-17 20:57:44 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,21 @@
 import os
 
+import requests
+from bs4 import BeautifulSoup
 from flask import Flask, render_template, redirect, url_for, request, session, flash
 from passlib.hash import sha512_crypt
 from sqlalchemy.orm import sessionmaker
 
-from bs4 import BeautifulSoup
-import requests
-
 from register import *
+from bookmark import *
 
 engine = create_engine('sqlite:///stronger.db', pool_pre_ping=True)
 Session = sessionmaker(bind=engine)
 db = Session()
+
+bookmark_engine = create_engine('sqlite:///bookmark.db', pool_pre_ping=True)
+Bookmark_Session = sessionmaker(bind=bookmark_engine)
+bookmark_db = Bookmark_Session()
 
 app = Flask(__name__, template_folder='templates')
 
@@ -20,8 +24,12 @@ app = Flask(__name__, template_folder='templates')
 def home(post=1):
     return render_template("index.html", username=post)
 
-@app.route('/featured_books')
-def featured_books(post=1):
+@app.route('/books')
+def books():
+    return render_template("books.html")
+
+@app.route('/sfwa_books')
+def sfwa_books():
     web_url = "https://www.sfwa.org/featured-books/"
     web_request = requests.get(web_url)
     soup = BeautifulSoup(web_request.text, 'html.parser')
@@ -38,7 +46,63 @@ def featured_books(post=1):
     for i in soup.findAll("article", class_="has-post-thumbnail"):
         links_to_web.append(i.div.h2.a.get('href'))
 
-    return render_template("featured_books.html", book_titles=book_titles, images=images, len=len(images), links_to_web=links_to_web)
+    return render_template("sfwa_books.html", book_titles=book_titles, images=images, len=len(images), links_to_web=links_to_web)
+
+@app.route('/yabook_books')
+def yabook_books():
+    web_url = "https://www.yabookscentral.com/"
+    web_request = requests.get(web_url)
+    soup = BeautifulSoup(web_request.text, 'html.parser')
+
+    book_titles = []
+    for i in soup.findAll("div", class_="jrListingTitle"):
+        book_titles.append(i.a.contents[0])
+
+    images = []
+    for i in soup.findAll("div", class_="jrListingThumbnail"):
+        images.append(i.a.img.get('src'))
+
+    links_to_web = []
+    for i in soup.findAll("div", class_="jrListingThumbnail"):
+        links_to_web.append("https://www.yabookscentral.com" + i.a.get('href'))
+
+    return render_template("yabook_books.html", book_titles=book_titles, images=images, len=len(images), links_to_web=links_to_web)
+
+@app.route('/bookchor_books')
+def bookchor_books():
+    web_url = "https://www.bookchor.com/Featured-Books"
+    web_request = requests.get(web_url)
+    soup = BeautifulSoup(web_request.text, 'html.parser')
+
+    book_titles = []
+    for i in soup.findAll("div", class_="product-item"):
+        book_titles.append(i.h3.a.contents[0])
+    book_titles = book_titles[:11]
+
+    images = []
+    for i in soup.findAll("div", class_="pi-img-wrapper"):
+        images.append(i.img.get('src'))
+    images = images[:11]
+
+    links_to_web = []
+    for i in soup.findAll("div", class_="product-item"):
+        links_to_web.append(i.h3.a.get('href'))
+    links_to_web = links_to_web[:11]
+
+    return render_template("bookchor_books.html", book_titles=book_titles, images=images, len=len(images), links_to_web=links_to_web)
+
+@app.route('/bookmark', methods=['GET', 'POST'])
+def bookmark():
+    if request.method == 'POST':
+        book_name = request.form.get('book_name')
+        book_image = request.form.get('book_image')
+
+        book = User_bookmarks(book_name=book_name, book_image=book_image)
+        bookmark_db.add(book)
+        bookmark_db.commit()
+        req = request.form
+        print(req)
+    return req
 
 @app.route('/login')
 def go_login():
@@ -47,7 +111,7 @@ def go_login():
 @app.route('/login')
 def login(post=1):
     if not session.get('logged_in'):
-        return render_template("login.html", username=post)
+        return render_template("login.html")
     else:
         return home(post)
 
